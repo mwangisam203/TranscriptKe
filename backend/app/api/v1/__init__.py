a77100f10248541b14beb7a9e74446f569ff7623
mwangisam203 <mwangisam203@gmail.com>
2026-07-10 14:18:19 -0400

    python file

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,3 +1,4 @@
 from app.api.v1.auth import router as auth_router
+from app.api.v1.institutions import router as institutions_router
 
-__all__ = ["auth_router"]
+__all__ = ["auth_router", "institutions_router"]
