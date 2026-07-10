162da0a9c65c5f260a8e784f024a9fe2873be3a2
mwangisam203 <mwangisam203@gmail.com>
2026-07-10 14:19:45 -0400

    mod main, added institution router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.api.v1.auth import router as auth_router
+from app.api.v1.institutions import router as institutions_router
 from app.core.config import settings
 
 
@@ -11,6 +12,7 @@ app = FastAPI(
 )
 
 app.include_router(auth_router, prefix="/api/v1")
+app.include_router(institutions_router, prefix="/api/v1")
 
 
 @app.get("/")
